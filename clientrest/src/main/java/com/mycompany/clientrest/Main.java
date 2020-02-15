4ecba2596b393e9803d509de86c0bcb879db8b90
matthaios_c <34523286+Xleesph@users.noreply.github.com>
2020-02-15 14:26:18 +0200

    Add files via upload

---
+++ clientrest/src/main/java/com/mycompany/clientrest/Main.java
@@ -0,0 +1,12 @@
+package com.mycompany.clientrest;
+
+public class Main {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Arxiki().setVisible(true);
+            }
+        });
+    }
+}
